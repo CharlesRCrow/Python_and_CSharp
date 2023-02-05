f6ad4b80c7bdab1b707bd88191281b8eea607f24
CharlesRCrow <Crow.charlesr@gmail.com>
2023-02-05 09:10:13 -0600

    Finished intial program

--- cas_check_sum.py
+++ cas_check_sum.py
@@ -6,5 +6,31 @@ def is_cas(cas):
     else:
         cas_list=[int(i) for i in cas_list  ]
     return True, cas_list
-    
-          
+
+def format_cas(cas_num, check_digit):
+    cas_num=''.join([str(i) for i in cas_num])
+    check_digit=str(check_digit)
+    first_half=cas_num[:-2]
+    second_half=cas_num[-2:]
+    return first_half+'-'+second_half+'-'+check_digit
+
+def check_sum(cas):
+    cas=is_cas(cas)
+    if cas is False:
+        return 'Not a Cas Number:'
+    cas=cas[1]
+    cas_num=cas[:-1]
+    check_digit=cas[-1]
+    rev_cas_num=cas_num[::-1]
+    i=1
+    total=0
+    clean_cas=format_cas(cas_num,check_digit)
+    for digit in rev_cas_num:
+        it=i*digit
+        i+=1
+        total+=it
+    return clean_cas,(total%10==check_digit)  
+
+cas_check=input('Enter CAS Number: ')
+result=check_sum(cas_check)
+print(result)
