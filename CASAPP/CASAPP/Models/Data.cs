a70d065944c224893b6c7234109c5056ce8b9cc9
CharlesRCrow <Crow.charlesr@gmail.com>
2023-10-15 15:13:59 -0500

    CASAPP

--- CASAPP/CASAPP/Models/Data.cs
+++ CASAPP/CASAPP/Models/Data.cs
@@ -19,8 +19,9 @@ public partial class Data : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=C:\\Users\\charl\\OneDrive\\Documents\\GitHub\\chemistry_tools\\CASAPP\\CASAPP\\CAS.db");
-
+    // => optionsBuilder.UseSqlite("Data Source=C:\\Users\\charl\\OneDrive\\Documents\\GitHub\\chemistry_tools\\CASAPP\\CASAPP\\CAS.db");
+    => optionsBuilder.UseSqlite(@"Data Source=\CAS.db");
+    //=> optionsBuilder.UseSqlite($@"Data Source={ System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)}\CAS.db");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Ca>(entity =>
