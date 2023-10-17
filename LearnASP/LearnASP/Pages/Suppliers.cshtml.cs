f58717ed4d576c95110e05efa086ca92c07ea3e1
CharlesRCrow <124510671+CharlesRCrow@users.noreply.github.com>
2023-10-17 22:07:28 +0000

    learnASP

---
+++ LearnASP/LearnASP/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using CAS_LINQ;
+
+namespace LearnASP.Web.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public IEnumerable<string>? Suppliers { get; set;}
+        
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind B2B - Suppliers";
+
+            Suppliers = new[]
+            {
+                "Alpha Co", "Beta Limited", "Gamma Corp"
+            };
+        }
+    }
+}
