8057da0f39eb19fe80d73fc72cb8c97ec616dd42
CharlesRCrow <124510671+CharlesRCrow@users.noreply.github.com>
2023-10-18 13:38:53 +0000

    nothing accomplished

---
+++ CASWEB_Project/CASWEB/Program.cs
@@ -0,0 +1,44 @@
+using CASWEB.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        //builder.Services.AddCasContext();
+        var app = builder.Build();
+
+        //private CasContext db;
+
+      //   public CasModel(CasContext injectedContext)
+      //   {
+      //       db = injectedContext;
+      //   }
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
+
+
